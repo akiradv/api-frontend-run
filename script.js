2faf6ca55065a9b7c1c159f9acd3d5f94ebcf8f0
Akira <ademarbernart@gmail.com>
2026-02-08 14:50:55 -0300

    frontend to run

---
+++ script.js
@@ -0,0 +1,85 @@
+const API = "http://localhost:3000";
+let token = localStorage.getItem("token");
+
+const authBox = document.getElementById("authBox");
+const postBox = document.getElementById("postBox");
+
+if (token) showPosts();
+
+function register() {
+  fetch(`${API}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    })
+  })
+  .then(r => r.json())
+  .then(data => {
+    authMsg.innerText = data.success ? "Registrado! Agora faça login." : "Erro ao registrar";
+  });
+}
+
+function login() {
+  fetch(`${API}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    })
+  })
+  .then(r => r.json())
+  .then(data => {
+    if (data.token) {
+      token = data.token;
+      localStorage.setItem("token", token);
+      showPosts();
+    } else {
+      authMsg.innerText = "Login inválido";
+    }
+  });
+}
+
+function showPosts() {
+  authBox.classList.add("hidden");
+  postBox.classList.remove("hidden");
+  loadPosts();
+}
+
+function createPost() {
+  fetch(`${API}/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + token
+    },
+    body: JSON.stringify({
+      title: title.value,
+      content: content.value
+    })
+  })
+  .then(() => {
+    title.value = "";
+    content.value = "";
+    loadPosts();
+  });
+}
+
+function loadPosts() {
+  fetch(`${API}/posts`)
+    .then(r => r.json())
+    .then(posts => {
+      feed.innerHTML = "";
+      posts.forEach(post => {
+        feed.innerHTML += `
+          <div class="card">
+            <strong>${post.title}</strong>
+            <p>${post.content}</p>
+            <small>${new Date(post.created_at).toLocaleString()}</small>
+          </div>
+        `;
+      });
+    });
+}
